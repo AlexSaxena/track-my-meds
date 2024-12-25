162b4643d270b5da168e8a686a9a7b341eccd7a7
Alex Saxena <saxenaalex@gmail.com>
2024-12-25 18:43:04 +0100

    Imports Medication Form

--- TrackMyMeds/src/routes/AddMedication.tsx
+++ TrackMyMeds/src/routes/AddMedication.tsx
@@ -1,3 +1,4 @@
+import MedicationForm from "@/components/ui/medicationForm";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/AddMedication")({
@@ -9,6 +10,7 @@ function AddMedication() {
     <main>
       <h1 className="text-xl font-bold">Add Medication</h1>
       <p>Use this form to add your medication details.</p>
+      <MedicationForm />
     </main>
   );
 }
