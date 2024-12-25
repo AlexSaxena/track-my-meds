63bc739afd63b53662611313685990e4f268c208
Alex Saxena <saxenaalex@gmail.com>
2024-12-25 18:44:35 +0100

    Updates form with on submit & destructured useForm with resolver for medication Schema.

--- TrackMyMeds/src/components/ui/medicationForm.tsx
+++ TrackMyMeds/src/components/ui/medicationForm.tsx
@@ -6,7 +6,24 @@ import { zodResolver } from "@hookform/resolvers/zod";
 type MedicationFormValues = z.infer<typeof medicationSchema>;
 
 function MedicationForm() {
-  return <div>MedicationForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<MedicationFormValues>({
+    resolver: zodResolver(medicationSchema),
+  });
+
+  const onSubmit = (data: MedicationFormValues) => {
+    console.log("Medication Data -> ", data);
+  };
+
+  return (
+    <div className="p-6 max-w-md mx-auto bg-white rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold mb-4">Add Medication</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4"></form>
+    </div>
+  );
 }
 
 export default MedicationForm;
