52135a1078602b5880fd352b7d76c92777be2507
Alex Saxena <saxenaalex@gmail.com>
2024-12-30 14:56:17 +0100

    Adds styled login page with an Google Sign-In option

--- TrackMyMeds/src/routes/Auth.tsx
+++ TrackMyMeds/src/routes/Auth.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import doctorImage from "/doctorImage.jpg";
+import GoogleIcon from "@/components/ui/googleIcon";
 
 export const Route = createFileRoute("/Auth")({
   component: AuthComponent,
@@ -7,8 +9,76 @@ export const Route = createFileRoute("/Auth")({
 function AuthComponent() {
   return (
     <main>
-      <h1 className="text-xl font-bold">Auth Page</h1>
-      <p>Login / Register Page</p>
+      <div className="py-16">
+        <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+          <img
+            src={doctorImage}
+            className="hidden lg:block lg:w-1/2 bg-cover"
+          />
+          <div className="w-full p-8 lg:w-1/2">
+            <h2 className="text-2xl font-semibold text-gray-700 text-center">
+              Track My Meds
+            </h2>
+            <p className="text-xl text-gray-600 text-center">Welcome back!</p>
+            <a
+              href="#"
+              className="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100"
+            >
+              <div className="px-4 py-3">
+                <GoogleIcon />
+              </div>
+              <h1 className="px-4 py-3 w-5/6 text-center text-gray-600 font-bold">
+                Sign in with Google
+              </h1>
+            </a>
+            <div className="mt-4 flex items-center justify-between">
+              <span className="border-b w-1/5 lg:w-1/4"></span>
+              <a
+                href="#"
+                className="text-xs text-center text-gray-500 uppercase"
+              >
+                or login with email
+              </a>
+              <span className="border-b w-1/5 lg:w-1/4"></span>
+            </div>
+            <div className="mt-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Email Address
+              </label>
+              <input
+                className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                type="email"
+              />
+            </div>
+            <div className="mt-4">
+              <div className="flex justify-between">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Password
+                </label>
+                <a href="#" className="text-xs text-gray-500">
+                  Forget Password?
+                </a>
+              </div>
+              <input
+                className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                type="password"
+              />
+            </div>
+            <div className="mt-8">
+              <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">
+                Login
+              </button>
+            </div>
+            <div className="mt-4 flex items-center justify-between">
+              <span className="border-b w-1/5 md:w-1/4"></span>
+              <a href="#" className="text-xs text-gray-500 uppercase">
+                or sign up
+              </a>
+              <span className="border-b w-1/5 md:w-1/4"></span>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
